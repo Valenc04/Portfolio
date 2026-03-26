495310bc64c03999aff0e98b9df2850afb11a039
Valenc04 <vcabanas@frba.utn.edu.ar>
2026-03-26 19:35:51 -0300

    Correcion

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex flex-col sm:flex-row items-center justify-between gap-6">
         <div className="flex flex-col gap-2 order-1 text-center sm:text-left">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold font-sans text-green-900">Valentín Cabanas</h1>
-          <h2 className="text-lg sm:text-xl font-extrabold font-sans text-green-800">Web Developer</h2>
+          <h2 className="text-lg sm:text-xl font-extrabold font-sans text-green-800">Systems Engineer Student</h2>
         </div>
         <Image
           className="w-full max-w-[200px] sm:max-w-[260px] md:max-w-[300px] rounded-full h-auto shrink-0 order-2"
